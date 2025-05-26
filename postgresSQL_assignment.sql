ad21dbb0068c528b2ea6cd0fe8b8c21c7e55cc10
md-ahmodullah <contactme.ahmod@gmail.com>
2025-05-26 21:51:24 +0600

    Created Tables

--- postgresSQL_assignment.sql
+++ postgresSQL_assignment.sql
@@ -1,2 +1,26 @@
 -- Active: 1748061296633@@127.0.0.1@5432@conservation_db
 
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    region VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER REFERENCES rangers(ranger_id) NOT NULL,
+    species_id INTEGER REFERENCES species(species_id) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    notes TEXT
+);
